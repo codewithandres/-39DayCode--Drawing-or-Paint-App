8f576ee24cfedb1c6d38c0be4868a37043fa29c0
Dize dev <axiozweb2022@gmail.com>
2024-05-04 22:48:29 -0500

    se le agrego la fucionalidad de debujar en el canva

--- js/app.js
+++ js/app.js
@@ -1,2 +1,31 @@
 
+const canvas = document.querySelector('canvas'),
+    ctx = canvas.getContext('2d');
 
+let isDrawin = false,
+    brushwidth = 5;
+
+window.addEventListener('load', () => {
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+});
+
+const startDrawign = () => {
+    isDrawin = true;
+
+    ctx.beginPath();
+    ctx.lineWidth = brushwidth;
+
+};
+
+const drawign = (e) => {
+    if (!isDrawin) return;
+
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+
+};
+
+canvas.addEventListener('mousedown', startDrawign)
+canvas.addEventListener('mousemove', drawign);
+canvas.addEventListener('mouseup', () => isDrawin = false);
